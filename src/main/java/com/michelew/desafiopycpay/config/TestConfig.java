e296b766c4c19a7b0d3ca59bcd19fe6aac98a865
michelebswm <michele.fenix.17@gmail.com>
2024-03-03 14:20:50 -0300

    fix: desativa teste

--- src/main/java/com/michelew/desafiopycpay/config/TestConfig.java
+++ src/main/java/com/michelew/desafiopycpay/config/TestConfig.java
@@ -15,27 +15,9 @@ import org.springframework.context.annotation.Profile;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private WalletRepository walletRepository;
-
     @Override
     public void run(String... args) throws Exception {
 
-        User u1 = new User(null, "Michele", "michele@gmail.com", "123456", TypePerson.FISICA, "12532564581", TypeUser.COMMON);
 
-        userRepository.save(u1);
-
-        System.out.println(u1);
-
-        Wallet w1 = new Wallet();
-        w1.setAccount("12389");
-        w1.setUser(u1);
-        w1.setBalance(100.0);
-
-        System.out.println(w1);
-        walletRepository.save(w1);
     }
 }
