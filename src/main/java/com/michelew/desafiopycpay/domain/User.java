68ed1991a228b80c99f31f77adcfdacd3eed1af6
michelebswm <michele.fenix.17@gmail.com>
2024-03-03 10:14:35 -0300

    fix: add column unique para email e numeroDocumento

--- src/main/java/com/michelew/desafiopycpay/domain/User.java
+++ src/main/java/com/michelew/desafiopycpay/domain/User.java
@@ -22,10 +22,12 @@ public class User {
 
     private String name;
 
+    @Column(unique = true)
     private String email;
 
     private String password;
 
+    @Column(unique = true)
     private String numeroDocumento;
 
     private TypePerson tipoPessoa;
